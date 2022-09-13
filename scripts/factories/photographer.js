fde3c9851cb496ba8a132f94c35d3be8a3a741d2
Luc Bruyant <lbruyant@outlook.fr>
2022-09-13 11:21:35 +0200

    Commentaires pour mieux comprendre le JS

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,50 +1,76 @@
+// CREATION DE LA FACTORY, AJOUT DE DONNES EN "DATA", PAR EXEMPLE LE TABLEAU DES PHOTOGRAPHES
 function photographerFactory(data) {
-    const { name, id, city, country, tagline, price, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    // SETUP PREALABLE 
+        // identification des data a extraire des tableaux passés en DATA
+        const { name, id, city, country, tagline, price, portrait } = data;
+        // construction d'une nouvelle data - le lien d'accès aux photos
+        const picture = `assets/photographers/${portrait}`;
 
+    // CREATION DES ARTICLES POUR LA PAGE D'ACCUEIL 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
 
-        const imgcontainer = document.createElement( 'div' );
-        imgcontainer.classList.add("profile-picture-container");
+        // CREATION DES ELEMENTS A IMPORTER DANS LA PAGE -- LES ELEMENTS NE SONT PAS ENCORE AJOUTES A LA PAGE
+            // Création de l'élément article -- pas encore rattaché au DOM 
+            const article = document.createElement( 'article' );
+            article.setAttribute("id", id);
+            article.setAttribute("onclick", "window.location='photographer.html?" + id + " ' ");
 
-        const imgfront = document.createElement( 'img' );
-        imgfront.setAttribute("src", picture);
-        imgfront.classList.add("profile-picture-front");
+            // Création de l'élément qui contient la profile picture et les effets visuels
+            const imgcontainer = document.createElement( 'div' );
+            imgcontainer.classList.add("profile-picture-container");
 
-        const imgbackfilter = document.createElement ('div');
-        imgbackfilter.classList.add("profile-picture-back-filter");
+                // Création de la profile picture
+                const imgfront = document.createElement( 'img' );
+                imgfront.setAttribute("src", picture);
+                imgfront.classList.add("profile-picture-front");
+                // Création du filtre blanc autour de l'image de fond
+                const imgbackfilter = document.createElement ('div');
+                imgbackfilter.classList.add("profile-picture-back-filter");
+                // Création de l'image de fond
+                const imgback = document.createElement( 'img' );
+                imgback.setAttribute("src", picture);
+                imgback.classList.add("profile-picture-background");
+ 
+            // Création du titre contenant le nom du photographe
+            const h2 = document.createElement( 'h2' );
+            h2.textContent = name;
 
-        const imgback = document.createElement( 'img' );
-        imgback.setAttribute("src", picture);
-        imgback.classList.add("profile-picture-background");
+            // Création du paragraphe contenant la localisation du photographe
+            const location = document.createElement ( 'p' );
+            location.textContent = city + ', ' + country;
+            location.classList.add("photographer_place");
 
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
+            // Création du paragraphe contenant la tagline du photographe     
+            const quote = document.createElement ( 'p' );
+            quote.textContent = tagline;
+            quote.classList.add("photographer_quote");
 
-        const location = document.createElement ( 'p' );
-        location.textContent = city + ', ' + country;
-        location.classList.add("photographer_place");
+            // Création du paragraphe contenant le pricing du photographe
+            const pricing = document.createElement ( 'p' );
+            pricing.textContent = price + "€/jour";
+            pricing.classList.add("photographer_price");
 
-        const quote = document.createElement ( 'p' );
-        quote.textContent = tagline;
-        quote.classList.add("photographer_quote");
+            // ORGANISATION DES ELEMENTS DE LA PHOTO AU SEIN DE LEUR CONTENEUR (front, filtre, back) -- ELEMENTS PAS AJOUTES A LA PAGE
+            imgcontainer.appendChild(imgfront);
+            imgcontainer.appendChild(imgbackfilter);
+            imgcontainer.appendChild(imgback)
 
-        const pricing = document.createElement ( 'p' );
-        pricing.textContent = price + "€/jour";
-        pricing.classList.add("photographer_price");
+            // ORGANISATION DES ELEMENTS CREES AU SEIN DE L'ARTICLE -- ELEMENTS PAS AJOUTES A LA PAGE
+            article.appendChild(imgcontainer);
+            article.appendChild(h2);
+            article.appendChild(location);
+            article.appendChild(quote);
+            article.appendChild(pricing);
 
-        article.appendChild(imgcontainer);
-        article.appendChild(h2);
-        article.appendChild(location);
-        article.appendChild(quote);
-        article.appendChild(pricing);
-        imgcontainer.appendChild(imgfront);
-        imgcontainer.appendChild(imgbackfilter);
-        imgcontainer.appendChild(imgback)
+            // VISUALISATION DE L'OBJET ARTICLE POUR CONTROLE
+            console.log(article);
 
-        return (article);
+            // L'OBJET ARTICLE EST CONSTRUIT MAIS PAS AJOUTE AU DOM 
+            return (article);
     }
+
+
+
     return { name, picture, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,26 @@
-//Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
+    const photographers = await fetch("data/photographers.json").then(photographers => photographers.json());
+    console.log(photographers);
+    // et bien retourner le tableau photographers seulement une fois
+    return photographers;
+    // return ({
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
